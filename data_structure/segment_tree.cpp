7858fc043b2b13e5b22ea6ee203d72c3b5b8bb91
Gordon <geniusgordon@gmail.com>
2016-01-29 17:37:33 +0800

    segment tree lazy modify

--- data_structure/segment_tree.cpp
+++ data_structure/segment_tree.cpp
@@ -5,13 +5,32 @@ using namespace std;
 struct SegmentTreeNode {
 	SegmentTreeNode *left, *right;
 	int L, R;
-	int value;
+	int value, tag;
 
 	SegmentTreeNode() {
 		left = right = NULL;
 		L = R = -1;
 		value = 0;
+        tag = 0;
 	}
+
+    void pull() {
+        value = left->value + right->value;
+    }
+
+    void push() {
+        if (!tag) return;
+        int M = (L + R) / 2;
+        if (left) {
+            left->tag += tag;
+            left->value += tag * (M - L);
+        }
+        if (right) {
+            right->tag += tag;
+            right->value += tag * (R - M);
+        }
+        tag = 0;
+    }
 };
 
 struct SegmentTree {
@@ -42,6 +61,10 @@ struct SegmentTree {
 		return _query(x, y, root);
 	}
 
+    void modify(int x, int y, int d) {
+        _modify(x, y, d, root);
+    }
+
 	void _build(int L, int R, SegmentTreeNode* node) {
 		node->L = L;
 		node->R = R;
@@ -59,7 +82,7 @@ struct SegmentTree {
 		_build(L, M, node->left);
 		_build(M, R, node->right);
 
-		node->value = node->left->value + node->right->value;
+		node->pull();
 	}
 
 	int _query(int x, int y, SegmentTreeNode* node) {
@@ -70,10 +93,28 @@ struct SegmentTree {
 		if (x <= node->L && node->R <= y)
 			return node->value;
 
+        node->push();
+
 		int a = _query(x, y, node->left);
 		int b = _query(x, y, node->right);
 
 		return a + b;
 	}
+
+    void _modify(int x, int y, int d, SegmentTreeNode* node) {
+        if (!node)
+            return;
+		if (y <= node->L || node->R <= x)
+            return;
+		if (x <= node->L && node->R <= y) {
+            node->tag += d;
+            node->value += d * (node->R - node->L);
+			return;
+        }
+        node->push();
+        _modify(x, y, d, node->left);
+        _modify(x, y, d, node->right);
+        node->pull();
+    }
 };
 
